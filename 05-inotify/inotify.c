436575772956856c6806b842a9d85d31357105d4
sharkmoos <muddy117@gmail.com>
2022-12-06 23:15:54 +0000

    day 5 completed

--- 05-inotify/inotify.c
+++ 05-inotify/inotify.c
@@ -47,16 +47,74 @@ struct {
 
 // We already know this macro from yesterday.
 #define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(*(arr)))
+#define BUFF_LEN 4096
 
 int main(void) {
     // We allocate a buffer to hold the inotify events, which are
     // variable in size.
-    void *buffer = malloc(4096);
-    if (!buffer) return -1;
+    void *buffer = malloc(BUFF_LEN);
+    if (!buffer)
+    {
+        perror("malloc");
+        exit(-1);
+    }
 
     // FIXME: Create Inotify Object (into inotify_fd)
+    int inotify_fd = inotify_init();
+    if (inotify_fd < 0)
+    {
+        perror("inotify_init");
+        return -1;
+    }
+
     // FIXME: Add new watch to that event (result into watch_fd)
+    int watch_fd = inotify_add_watch(inotify_fd, ".", IN_OPEN | IN_ACCESS | IN_CLOSE);
+    if (watch_fd < 0)
+    {
+        perror("inotify_add_watch");
+        return -1;
+    }
+
     // FIXME: Use read() and the buffer to retrieve results from the inotify_fd
+    while ( true )
+    {
+        // the buffer could contain multiple inotify events
+        int read_size = read(inotify_fd, buffer, BUFF_LEN);
+        if ( read_size < 0)
+        {
+            perror("inotify-read");
+            return -1;
+            break;
+        }
+        // inotify events are variable in size, do some cleverness to iterate over them
+        for (struct inotify_event *event = buffer;
+            (char *)event < (char *)buffer + read_size; // linter is going to hate this...
+            event = (struct inotify_event*)event + sizeof(struct inotify_event) + event->len
+                    )
+        {
+            // not all inotify events will have a name (file name). Those are directory events
+            if ( event -> len != 0 )
+            {
+                printf("Inotify event: [%s]    ", event->name);
+            }
+            else
+            {
+                printf("Inotify event: [.]    ");
+            }
+
+            char *deliminator = " | ";
+            for (int i = 0; i < ARRAY_SIZE(inotify_event_flags); i++)
+            {
+                if (event->mask & inotify_event_flags[i].mask)
+                {
+                    printf("%s%s", deliminator, inotify_event_flags[i].name);
+                }
+            }
+            printf("\n");
+
+        }
+    }
+
     // As we are nice, we free the buffer again.
     free(buffer);
     return 0;
