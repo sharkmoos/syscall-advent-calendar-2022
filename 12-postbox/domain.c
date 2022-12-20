3759a2cb6a1b8331a2dbe6ac18a00e292887d527
sharkmoos <muddy117@gmail.com>
2022-12-20 18:00:34 +0000

    Day 12 submission
    Program doesnt seem to work to be honest... Ended up rewriting the entire thing to look more like the solution provided but still no better. Will test on a full VM some time

--- 12-postbox/domain.c
+++ 12-postbox/domain.c
@@ -1,12 +1,102 @@
-int domain_prepare(int epoll_fd) {
+/* Created the socket at /tmp/socket rather than ./socket because the
+ * Windows file system does not support sockets so using WSL
+ * */
+
+int domain_prepare(int epoll_fd)
+{
     printf("... by socket: echo 2 | nc -U socket\n");
-    return -1;
+    // open the socket file descriptor
+    int socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (socket_fd < 0)
+    {
+        printf("Socket open failed\n");
+        return -1;
+    }
+
+    // create the socket struct
+    char *socket_name = "socket";
+    struct sockaddr_un server_socket_addr;
+    strcpy(server_socket_addr.sun_path, socket_name);
+    server_socket_addr.sun_family = AF_UNIX;
+
+    // unlink the socket
+    if ( unlink(socket_name) < 0 && errno != ENOENT )
+    {
+        printf("unlink socket failed\n");
+        return -1;
+    }
+
+    // bind the socket to the file descriptor
+    if ( bind(socket_fd, (struct sockaddr *)&server_socket_addr, sizeof(server_socket_addr)) == -1 )
+    {
+        printf("bind socket failed\n");
+        return -1;
+    }
+
+    // listen for connections
+    if ( listen(socket_fd, 10) < 0 )
+    {
+        printf("listen socket failed\n");
+        return -1;
+    }
+
+    // add the fifo fd to the epoll list
+    epoll_add(epoll_fd, socket_fd, EPOLLIN);
+    return socket_fd;
 }
 
 
-void domain_accept(int epoll_fd, int sock_fd, int events) {
+void domain_accept(int epoll_fd, int sock_fd, int events)
+{
+    // accept the connection and add to the epoll
+    int client_fd = accept(sock_fd, NULL, NULL);
+    if (client_fd < 0)
+    {
+        printf("accept failed\n");
+        return;
+    }
+    epoll_add(epoll_fd, client_fd, EPOLLIN);
 }
 
 
-void domain_recv(int epoll_fd, int sock_fd, int events) {
+void domain_recv(int epoll_fd, int sock_fd, int events)
+{
+    static char buf[1024];
+    if ( EPOLLIN & events )
+    {
+        // read the message from the socket
+        int len = recv(sock_fd, buf, sizeof(buf), 0);
+        if (  len < 0 )
+        {
+           printf("recv failed\n");
+           return;
+        }
+
+        while ( len > 0 && buf[len-1] == '\n' )
+            len--;
+        buf[len-1] = '\0';
+
+        // retrieve the socket options (getsockopt) and print the message
+        struct ucred ucred; // struct to hold the socket options
+        socklen_t ucred_len = sizeof(ucred);
+        if ( getsockopt(sock_fd, SOL_SOCKET, SO_PEERCRED, &ucred, &ucred_len) < 0 )
+        {
+            printf("getsockopt failed\n");
+            return;
+        }
+
+        printf("Received request from pid:%d uid:%d gid:%d: %s\n", ucred.pid, ucred.uid, ucred.gid, buf);
+        epoll_del(epoll_fd, sock_fd);
+        close(sock_fd);
+
+    }
+    else if ( EPOLLHUP & events )
+    {
+        epoll_del(epoll_fd, sock_fd);
+        close(sock_fd);
+    }
+    else
+    {
+        printf("Unknown event\n");
+    }
 }
--- 12-postbox/fifo.c
+++ 12-postbox/fifo.c
@@ -1,8 +1,70 @@
+/* Created the fifo at /tmp/fifo rather than ./fifo because the
+ * Windows file system does not support named pipes so using WSL.
+ * */
 
-int fifo_prepare(int epoll_fd) {
+
+int fifo_prepare(int epoll_fd)
+{
     printf("... by fifo:   echo 1 > fifo\n");
-    return -1;
+
+    // unlink the file
+    if ( unlink("/tmp/fifo") < 0 && errno != ENOENT )
+    {
+        printf("unlink fifo failed\n");
+        return -1;
+    }
+
+    // create the fifo using mknod
+    if ( mknod("/tmp/fifo", S_IFIFO | 0666, 0) < 0 )
+    {
+        printf("mknod fifo failed\n");
+        return -1;
+    }
+
+    // create a file descriptor for the fifo
+    int fifo_fd = open("/tmp/fifo", O_RDONLY | O_NONBLOCK);
+    if ( fifo_fd < 0 )
+    {
+        printf("open fifo failed\n");
+        return -1;
+    }
+
+    epoll_add(epoll_fd, fifo_fd, EPOLLIN);
+    return fifo_fd;
 }
 
-void fifo_handle(int epoll_fd, int fifo_fd, int events) {
+void fifo_handle(int epoll_fd, int fifo_fd, int events)
+{
+    static char buf[1024];
+    if (events & EPOLLIN)
+    {
+        // read from the fifo
+        ssize_t len = read(fifo_fd, buf, sizeof(buf));
+        if (len < 0)
+        {
+            printf("read fifo failed");
+            return;
+        }
+        else if (len > 1)
+        {
+            while (len > 1 && buf[len - 1] == '\n')
+                len--;
+            buf[len] = '\0';
+            printf("fifo: %s", buf);
+        }
+        else if ( len == 0 )
+        {
+            // the fifo has been closed
+            goto close;
+        }
+    }
+    else if (events & EPOLLHUP)
+    {
+        close:
+            epoll_del(epoll_fd, fifo_fd);
+            close(fifo_fd);
+
+            fifo_fd = open("/tmp/fifo", O_RDONLY | O_NONBLOCK);
+            epoll_add(epoll_fd, fifo_fd, EPOLLIN);
+    }
 }
